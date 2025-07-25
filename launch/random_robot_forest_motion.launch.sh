d263a9338b2e811a39dd01c66a5497839f130c8b
3ernhard <bernhard-hoerl@gmx.at>
2025-07-25 11:18:13 +0200

    added temporary fix for broken ros_bridge (gazebosim master)

--- launch/random_robot_forest_motion.launch.sh
+++ launch/random_robot_forest_motion.launch.sh
@@ -4,10 +4,9 @@
 
 ros2 launch ros_gz_bridge ros_gz_bridge.launch.py bridge_name:=ros_gz_bridge config_file:=config/bridges.yaml &
 
-python3 python/force_bar_topic_merger.py 0 &
-python3 python/force_bar_topic_merger.py 1 &
-python3 python/force_bar_topic_merger.py 2 &
-python3 python/force_bar_topic_merger.py 3 &
-python3 python/force_bar_topic_merger.py 4 &
+for i in {0..4}; do
+    python3 python/force_bar_topic_merger.py $i &
+done
+gz topic -t '/cmd_vel' -m gz.msgs.Twist -p 'linear: {x: 0.5}, angular: {z: 0.00}'
 python3 python/random_robot_forest_motion.py
 kill $(jobs -p)

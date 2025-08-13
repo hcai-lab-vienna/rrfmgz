019465ae6b9159e361d6c01cb886ccb1c3e1891f
3ernhard <bernhard-hoerl@gmx.at>
2025-08-13 17:37:25 +0200

    fix 4 border maneuver PART 1

--- src/rrfm/rrfm/rrfm_node.py
+++ src/rrfm/rrfm/rrfm_node.py
@@ -14,6 +14,7 @@
 
 
 import random
+import math
 from pathlib import Path
 from datetime import datetime
 
@@ -21,7 +22,7 @@ import rclpy
 from rclpy.node import Node
 
 # from sensor_msgs.msg import Imu
-from geometry_msgs.msg import Twist, Pose
+from geometry_msgs.msg import Twist, Pose, Quaternion
 from std_msgs.msg import String
 
 
@@ -29,18 +30,27 @@ SEGMENTS:int = 5
 SEGMENT_THRESHOLDS:list[float] = [200, 200, 200, 200, 200]
 assert len(SEGMENT_THRESHOLDS) == SEGMENTS
 STARTING_VELOCITY:float = 0.5
-BOX_SIZE:tuple[float, float] = (50.0, 50.0)
+BOX_SIZE:tuple[float, float] = (30.0, 30.0)
+RECORD:bool = False
 
 
-def COMMANDS(sn:int=3, val:float=0.0) -> list[tuple]:
+def COLLISION_COMMANDS(sn:int=3, val:float=0.0) -> list[tuple]:
     "motion routine when collision happens"
     a1 = 0.5 * (-1 if sn < 3 else random.choice([-1, 1]) if sn == 3 else 1)
     d1 = random.uniform(2, 6)
-    #     linear, angular,   duration
     return [
-        (   -0.5,       0,        1.5),
-        (      0,      a1,         d1),
-        (    0.5,       0,          1)
+        # linear, angular,   duration
+        (   -0.5,     0.0,        1.5),
+        (    0.0,      a1,         d1),
+        (    0.5,     0.0,        0.1)
+    ].copy()
+
+
+def WALL_COMMANDS(pos_x:float=0.0, pos_y:float=0.0, heading:float=0.0, out_of_bounds:bool=True) -> list[tuple]:
+    "motion routine when out of bounds"
+    return [
+        # linear, angular,   duration
+
     ].copy()
 
 
@@ -48,20 +58,39 @@ class RandomRobotForestMotion(Node):
 
     def __init__(self):
         super().__init__('random_robot_forest_motion')
-        self.starting_position = ()
         self.command_stack = []
+        self.out_of_bounds = False
+        self.pose_x = 0.0
+        self.pose_y = 0.0
+        self.yaw = 0.0
         self.timer = self.create_timer(0, lambda *args, **kwargs: None)
         self.timer.cancel()
         self.cmd_vel = self.create_publisher(Twist,'/cmd_vel', 10)
         self.collision = self.create_subscription(String, '/merged_force_topic', self.collision_callback, 10)
         self.pose = self.create_subscription(Pose, '/scout/pose', self.pose_callback, 10)
         # for each run a day create a new save file
-        self.save_file = ''
-        base_str = r'data/recored_positions_' + datetime.now().strftime(r"%y%m%d") + r'_{}.csv'
-        for i in range(1, 1000):
-            self.save_file = base_str.format(f"{i:03d}")
-            if not Path(self.save_file).exists():
-                break
+        if RECORD:
+            self.save_file = ''
+            base_str = r'data/recored_positions_' + datetime.now().strftime(r"%y%m%d") + r'_{}.csv'
+            for i in range(1, 1000):
+                self.save_file = base_str.format(f"{i:03d}")
+                if not Path(self.save_file).exists():
+                    break
+
+    @staticmethod
+    def quaternion_to_yaw(q:Quaternion) -> float:
+        "Converts a quaternion into yaw (rotation about Z axis). Returns yaw in radians."
+        siny_cosp = 2*(q.w*q.z + q.x*q.y)
+        cosy_cosp = 1 - 2*(q.y**2 +q.z**2)
+        return math.atan2(siny_cosp, cosy_cosp)
+
+    @staticmethod
+    def angle_to_center(pos_x:float, pos_y:float, yaw:float) -> float:
+        "Compute the angle pointing towards the center of the box."
+        direction_to_center = math.atan2(-pos_x, -pos_y)  # vector from robot to center
+        heading = direction_to_center - yaw
+        heading = (heading + math.pi)%(2*math.pi) - math.pi  # normalize
+        return heading  # in radians
 
     def move(self, linear:float, angular:float):
         "Move the robot with current values of self.linear & self.angular respectively."
@@ -94,21 +123,20 @@ class RandomRobotForestMotion(Node):
             self.get_logger().info(log_msg)
         return return_val
 
-    def detect_wall(self, pos_x:float, pos_y:float) -> bool:
+    def detect_wall(self) -> bool:
+        "Returns True if outside of BOX_SIZE."
         return_val = False
-        if not self.starting_position:
-            self.starting_position = (pos_x, pos_y)
-            self.get_logger().info(f"START: x:{pos_x:.2f} y:{pos_y:.2f}")
+        if abs(self.pose_x) >= BOX_SIZE[0]/2 or abs(self.pose_y) >= BOX_SIZE[1]/2:
+            log_msg = f"WALL pose_x:{self.pose_x:.2f} pose_y:{self.pose_y:.2f} yaw:{self.yaw:.2f}"
+            if self.command_stack:
+                log_msg += ' (ignored)'
+            else:
+                return_val = True
+            self.get_logger().info(log_msg)
+            # set out of bounds check
+            self.out_of_bounds = True
         else:
-            pos_x = self.starting_position[0] - pos_x
-            pos_y = self.starting_position[1] - pos_y
-            if abs(pos_x) >= BOX_SIZE[0]/2 or abs(pos_y) >= BOX_SIZE[1]/2:
-                log_msg = f"WALL x:{pos_x:.2f} y:{pos_y:.2f}"
-                if self.command_stack:
-                    log_msg += ' (ignored)'
-                else:
-                    return_val = True
-                self.get_logger().info(log_msg)
+            self.out_of_bounds = False
         return return_val
 
     def command_callback(self):
@@ -128,17 +156,21 @@ class RandomRobotForestMotion(Node):
         val = float(val)
         if self.detect_collision(sn, val, SEGMENT_THRESHOLDS[sn]):
             self.stop()
-            self.command_stack = COMMANDS(sn, val)
+            self.command_stack = COLLISION_COMMANDS(sn, val)
         if self.timer.is_canceled():
             self.command_callback()
 
     def pose_callback(self, msg:Pose):
         "Enforce map boundaries and record position data."
-        if self.detect_wall(msg.position.x, msg.position.y):
-            self.stop()
-            self.command_stack = COMMANDS()
-        with open(self.save_file, 'a') as f:
-            f.write(f"{msg.position.x},{msg.position.y},{len(self.command_stack)}\n")
+        self.pose_x = msg.position.x
+        self.pose_y = msg.position.y
+        self.yaw = self.quaternion_to_yaw(msg.orientation)
+        if self.detect_wall() and not self.command_stack:
+            heading = self.angle_to_center(self.pose_x, self.pose_y, self.yaw)
+            self.command_stack = WALL_COMMANDS(self.pose_x, self.pose_y, heading, self.out_of_bounds)
+        if RECORD:
+            with open(self.save_file, 'a') as f:
+                f.write(f"{self.pose_x},{self.pose_y},{len(self.command_stack)},{self.yaw}\n")
 
 
 def main(args=None):

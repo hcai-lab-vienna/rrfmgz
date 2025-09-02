c001a6eb57e74d22a1c6ce049cd848cb01443ec7
3ernhard <bernhard-hoerl@gmx.at>
2025-09-02 16:08:36 +0200

    added frame_id for ros2 gnss msg

--- src/gnss/gnss/gnss_node.py
+++ src/gnss/gnss/gnss_node.py
@@ -27,6 +27,7 @@ class GNSS(Node):
             self.ser_cmd.gnss_up()
         self.latlong = None
         self.old_latlong = None
+        self.frame_id = 0
 
     def __del__(self):
         if self.ser_dat:
@@ -35,13 +36,15 @@ class GNSS(Node):
             self.ser_cmd.close()
 
     def timer_callback(self):
-        msg = NavSatFix()
-        msg.header.stamp = self.get_clock().now().to_msg()
         latlong = self.ser_dat.gnss_latlong
         if latlong:
             self.latlong = latlong
         if self.latlong and self.latlong != self.old_latlong:
             self.old_latlong = self.latlong
+            msg = NavSatFix()
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.header.frame_id = self.frame_id
+            self.frame_id += 1
             msg.latitude, msg.longitude = self.latlong
             self.publisher.publish(msg)
 

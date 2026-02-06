18dd198d04d63a98463df1b146d946ca1a703144
3ernhard <bernhard-hoerl@gmx.at>
2026-02-06 12:44:31 +0100

    Added shell env variable RRFM_SEED for fixed randomnes and option to always use a random rotation direction.

--- src/rrfm/rrfm/rrfm_node.py
+++ src/rrfm/rrfm/rrfm_node.py
@@ -13,27 +13,28 @@
 # limitations under the License.
 
 
-import random
 import math
-from glob import glob
+import os
+import random
 from datetime import datetime
+from glob import glob
 
 import rclpy
-from rclpy.node import Node
 
 # from sensor_msgs.msg import Imu
-from geometry_msgs.msg import Twist, Pose, Quaternion
+from geometry_msgs.msg import Pose, Quaternion, Twist
+from rclpy.node import Node
 from std_msgs.msg import String
 
 
-def quaternion_to_yaw(q:Quaternion) -> float:
+def quaternion_to_yaw(q: Quaternion) -> float:
     "Converts a quaternion into yaw (rotation about Z axis). Returns yaw in radians."
-    siny_cosp = 2*(q.w*q.z + q.x*q.y)
-    cosy_cosp = 1 - 2*(q.y**2 +q.z**2)
+    siny_cosp = 2 * (q.w * q.z + q.x * q.y)
+    cosy_cosp = 1 - 2 * (q.y**2 + q.z**2)
     return math.atan2(siny_cosp, cosy_cosp)
 
 
-def angle_to_center(pose:tuple) -> float:
+def angle_to_center(pose: tuple) -> float:
     """
     Angle to the origin.
     Why -y and -x?
@@ -43,20 +44,20 @@ def angle_to_center(pose:tuple) -> float:
     return math.atan2(-pose[1], -pose[0])
 
 
-def normalize_angle(angle:float) -> float:
+def normalize_angle(angle: float) -> float:
     "To prevent yaw angle wrapping from -pi to pi, causes discontinuities in rotation"
     return (angle + math.pi) % (2 * math.pi) - math.pi
 
 
 class RandomRobotForestMotion(Node):
-
-    segment_thresholds:list[float] = [200, 200, 200, 200, 200]
-    box_size:tuple[float, float] = (40.0, 40.0)
-    record:bool = True
-    recover_from_stand_still:bool = True
+    segment_thresholds: list[float] = [200, 200, 200, 200, 200]
+    box_size: tuple[float, float] = (40.0, 40.0)
+    record: bool = True
+    recover_from_stand_still: bool = True
+    always_random_rotation: bool = True
 
     def __init__(self):
-        super().__init__('random_robot_forest_motion')
+        super().__init__("random_robot_forest_motion")
         # command_stack will be executed by the robot from top to bottom,
         # each command is a tuple of (linear speed, angular speed, duration in seconds).
         self.command_stack = []
@@ -74,28 +75,35 @@ class RandomRobotForestMotion(Node):
         self.immediate_collision_timer.cancel()
         self.immediate_collision = False
         # ros callbacks
-        self.cmd_vel = self.create_publisher(Twist,'/cmd_vel', 10)
-        self.collision = self.create_subscription(String, '/merged_force_topic', self.collision_callback, 10)
-        self.pose = self.create_subscription(Pose, '/scout/pose', self.pose_callback, 10)
+        self.cmd_vel = self.create_publisher(Twist, "/cmd_vel", 10)
+        self.collision = self.create_subscription(
+            String, "/merged_force_topic", self.collision_callback, 10
+        )
+        self.pose = self.create_subscription(
+            Pose, "/scout/pose", self.pose_callback, 10
+        )
         # for each run a day create a new save file
         if self.record:
-            base_str = f'data/recored_positions_{datetime.now().strftime(r"%y%m%d")}_'
-            file_list = sorted(glob(base_str + '*'))
+            base_str = f"data/recored_positions_{datetime.now().strftime(r'%y%m%d')}_"
+            file_list = sorted(glob(base_str + "*"))
             if file_list:
-                file_nr = 1 + int(file_list[-1].replace(base_str, '').split('.')[0])
+                file_nr = 1 + int(file_list[-1].replace(base_str, "").split(".")[0])
             else:
                 file_nr = 1
-            self.save_file = base_str + f'{file_nr:03d}.csv'
+            self.save_file = base_str + f"{file_nr:03d}.csv"
 
-    def collision_commands(self, sn:int=3) -> list[tuple]:
+    def collision_commands(self, sn: int = 3) -> list[tuple]:
         "motion routine when collision happens"
-        a2 = 0.5 * (-1 if sn < 3 else random.choice([-1, 1]) if sn == 3 else 1)
+        if self.always_random_rotation:
+            a2 = 0.5 * random.choice([-1, 1])
+        else:
+            a2 = 0.5 * (-1 if sn < 3 else random.choice([-1, 1]) if sn == 3 else 1)
         d2 = random.uniform(2, 6)
         return [
             # linear, angular, duration
-            (   -0.5,     0.0,      1.5),
-            (    0.0,      a2,       d2),
-            (    0.5,     0.0,      0.1)
+            (-0.5, 0.0, 1.5),
+            (0.0, a2, d2),
+            (0.5, 0.0, 0.1),
         ].copy()
 
     def wall_commands(self) -> list[tuple]:
@@ -106,20 +114,20 @@ class RandomRobotForestMotion(Node):
             sign = angle // abs(angle)
         except ZeroDivisionError:
             sign = 1
-        heading = abs(angle)/math.pi
+        heading = abs(angle) / math.pi
         if heading > 0.1:
-            d1 = 5*heading
+            d1 = 5 * heading
             l2 = 0.0
         else:
             d1 = 0.0
             l2 = 0.5
         return [
             # linear,  angular, duration
-            (    0.0, sign*0.5,       d1),
-            (     l2,      0.0,      0.1)
+            (0.0, sign * 0.5, d1),
+            (l2, 0.0, 0.1),
         ].copy()
 
-    def move(self, linear:float, angular:float):
+    def move(self, linear: float, angular: float):
         "Move the robot with current values of self.linear & self.angular respectively."
         msg = Twist()
         msg.linear.x = float(linear)
@@ -142,20 +150,22 @@ class RandomRobotForestMotion(Node):
         "Prevent to many collision detections in a short amount of time."
         if not self.immediate_collision:
             self.immediate_collision = True
-            self.immediate_collision_timer = self.create_timer(0.5, self.reset_immediate_collision)
-    
+            self.immediate_collision_timer = self.create_timer(
+                0.5, self.reset_immediate_collision
+            )
+
     def reset_immediate_collision(self):
         "Allows collision detection again, should only be called by set_immediate_collision."
         self.immediate_collision_timer.cancel()
         self.immediate_collision = False
 
-    def detect_collision(self, sn:int, val:float, threshold:float) -> bool:
+    def detect_collision(self, sn: int, val: float, threshold: float) -> bool:
         "Returns True if val exceeds threshold and no maneuver is in progress, False otherwise."
         return_val = False
         if abs(val) > threshold:
             log_msg = f"COLL sn:{sn} val:{val:.2f}"
             if self.immediate_collision:
-                log_msg += ' (ignored)'
+                log_msg += " (ignored)"
             else:
                 self.set_immediate_collision()
                 return_val = True
@@ -165,10 +175,13 @@ class RandomRobotForestMotion(Node):
     def detect_wall(self) -> bool:
         "Returns True if outside of BOX_SIZE."
         return_val = False
-        if abs(self.pose[0]) >= self.box_size[0]/2 or abs(self.pose[1]) >= self.box_size[1]/2:
+        if (
+            abs(self.pose[0]) >= self.box_size[0] / 2
+            or abs(self.pose[1]) >= self.box_size[1] / 2
+        ):
             log_msg = f"WALL pose_x:{self.pose[0]:.2f} pose_y:{self.pose[1]:.2f} yaw:{self.yaw:.2f}"
             if self.command_stack:
-                log_msg += ' (ignored)'
+                log_msg += " (ignored)"
             else:
                 return_val = True
             self.get_logger().info(log_msg)
@@ -191,9 +204,9 @@ class RandomRobotForestMotion(Node):
             # stand still prevention
             self.command_stack = [(0.5, 0.0, 10.0)]
 
-    def collision_callback(self, msg:String):
+    def collision_callback(self, msg: String):
         "Detect collisions and calculates movement."
-        sn, val = msg.data.split(' ')
+        sn, val = msg.data.split(" ")
         sn = int(sn)
         val = float(val)
         if self.detect_collision(sn, val, self.segment_thresholds[sn]):
@@ -202,7 +215,7 @@ class RandomRobotForestMotion(Node):
         if self.command_timer.is_canceled():
             self.command_callback()
 
-    def pose_callback(self, msg:Pose):
+    def pose_callback(self, msg: Pose):
         "Enforce map boundaries and record position data."
         self.pose = msg.position.x, msg.position.y
         self.yaw = quaternion_to_yaw(msg.orientation)
@@ -211,11 +224,14 @@ class RandomRobotForestMotion(Node):
             self.stop()
             self.command_stack = self.wall_commands()
         if self.record:
-            with open(self.save_file, 'a') as f:
-                f.write(f"{self.pose[0]},{self.pose[1]},{self.yaw},{int(self.immediate_collision)},{int(self.out_of_bounds)}\n")
+            with open(self.save_file, "a") as f:
+                f.write(
+                    f"{self.pose[0]},{self.pose[1]},{self.yaw},{int(self.immediate_collision)},{int(self.out_of_bounds)}\n"
+                )
 
 
 def main(args=None):
+    random.seed(os.environ.get("RRFM_SEED", default=None))
     rclpy.init(args=args)
     node = RandomRobotForestMotion()
     rclpy.spin(node)
@@ -223,5 +239,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

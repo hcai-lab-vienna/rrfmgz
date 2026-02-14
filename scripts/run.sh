02590dd55f1a0545296d16e8e97626e440b28ff6
3ernhard <bernhard-hoerl@gmx.at>
2026-02-14 09:43:29 +0100

    run.sh takes seeds from cmdline args

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ source install/setup.bash
 
 export RRFM_TTL=600
 
-for seed in alf0 alf1 alf2 alf3 alf4 alf5 alf6 alf7 alf8 alf9 blf0 blf1 blf2 blf3 blf4 blf5 blf6 blf7 blf8 blf9 clf0 clf1 clf2 clf3 clf4 clf5 clf6 clf7 clf8 clf9 dlf0 dlf1 dlf2 dlf3 dlf4 dlf5 dlf6 dlf7 dlf8 dlf9 flf0 flf1 flf2 flf3 flf4 flf5 flf6 flf7 flf8 flf9
+for seed in "$@"
 do
 	for val in '0' '1'
 	do
